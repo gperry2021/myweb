3f21c08b28e3c71014a522ca3e46230dfa1ce9bf
Gabe <swatforce77@gmail.com>
2021-01-14 13:08:46 -0500

    changed the code.js and removed document.writeIn() and changed it to document.write()

--- code.js
+++ code.js
@@ -6,15 +6,15 @@ var word = "bottles";
 						
 					console.log(count + " " + word + " of pop on the wall");
 					
-					document.writeln("<p>" + count + " " + word + " of pop on the wall</p>");
+					document.write("<p>" + count + " " + word + " of pop on the wall</p>");
 							
 					console.log(count + " " + word + " of pop,");
 							
-					document.writeln("<p>" + count + " " + word + " of pop,</p>");
+					document.write("<p>" + count + " " + word + " of pop,</p>");
 							
 					console.log("Take one down, pass it around,");
 							
-					document.writeln("<p>Take one down, pass it around,</p>");
+					document.write("<p>Take one down, pass it around,</p>");
 							
 					count = count - 1;
 							
@@ -22,13 +22,13 @@ var word = "bottles";
 							
 					console.log(count + " " + word + " of pop on the wall.");
 								
-					document.writeln("<p>" + count + " " + word + " of pop on the wall.</p>");
+					document.write("<p>" + count + " " + word + " of pop on the wall.</p>");
 								
 				}	else {
 							
 						console.log("No more " + word + " of pop on the wall.");
 								
-					document.writeln("<p>No more " + word + " of pop on the wall.</p>");
+					document.write("<p>No more " + word + " of pop on the wall.</p>");
 							
 				}
 					
