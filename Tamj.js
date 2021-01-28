31673356fc59c0c2d876e6962c82698d58f1eee3
Gabe <swatforce77@gmail.com>
2021-01-28 13:26:53 -0500

    fixed up tamj.js and fixed up javascript.css

--- Tamj.js
+++ Tamj.js
@@ -14,7 +14,7 @@ function thingamajig(size) {
 	}else if (size == 1) {
 		display("thunk");	//Checks to see if size is equal to 1 if not moves on
 	}else{
-		while (size > 1) {		//if size is greater than 1 it does facky multiplied by size 
+		while (size > 1) {		//if size is greater than 1 it does facky multiplied by size continues until size is 1
 			facky = facky * size;
 			size = size -1;		//subtracts size by 1
 		}
@@ -24,11 +24,13 @@ function thingamajig(size) {
 
 function display(output) {
 	console.log(output);
+	document.write(output + " "); //displays clank or think or clunk the number of times that output is
 	clunkCounter = clunkCounter + 1;
 }
 //If the number is less than 0 it will display Clank
 //If the number is equal to 1 it will be Thunk
 //If the number is greater than 1 it will be Clunk
 var clunkCounter = 0;
-thingamajig(2);	//The number that will display what output it is
+thingamajig(3);	//The number that will display what output it is
 console.log(clunkCounter);
+document.write(clunkCounter);
